744939568db90603c699208dd2ccde9b5c2ab5a0
frankovacevich <32471867+frankovacevich@users.noreply.github.com>
2018-11-15 20:01:12 +0100

    Update model_lib.py

--- config/model_lib.py
+++ config/model_lib.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-r"""Constructs model, inputs, and training environment."""
+"""Constructs model, inputs, and training environment."""
 
 from __future__ import absolute_import
 from __future__ import division
